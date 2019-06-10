6890c4853995e0026827218db7353547cda94701
jchavezrol <jchavezrol29@gmail.com>
2019-06-10 00:30:52 -0500

    Avance de login

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@
 const btn = document.getElementById("btn");
 //tomando lo que ingresa el usuario 
 const clave = document.getElementById("clave");
-// const string = clave.toUpperCase(); 
-btn.addEventListener('click', ()=>{
+const intentos = 0;
 
-    if(clave.value=="LABORATORIA" ){
+btn.addEventListener('click', ()=>{
+    if(clave.value == "LABORATORIA" ){
       document.getElementById('acceso').style.display = 'none';
       document.getElementById('ingreso').style.display='block';
-    }else{
-       alert('fallaste');
+}
+    else if (clave.value != "LABORATORIA"){
+    if(intentos.value=intentos+1){
+         alert('¡Error!, te quedan 2 intentos');
     }
-   
+    else if (intentos.value=intentos+2){
+        alert('¡Error!, te queda 1 intento');
+    }
+      else (intentos.value=intentos+3){
+        alert('Ya utilizaste todos tus intentos, en este momento, no podrás seguir');    
+  }
+}
 });
 
 
