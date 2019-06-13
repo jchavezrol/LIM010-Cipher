419470be39a4c395cabdd98b80c43c60c86cad49
jchavezrol <jchavezrol29@gmail.com>
2019-06-13 12:26:10 -0500

    avanzando el cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,9 @@
 window.cipher = {
  
   encode: (offset, string) => {
-    let result = "";
-    result =string.charCodeAt(0)-65+parseInt(offset,10)+65;
-    return String.fromCharCode(result);
+    let resultado = "";
+    resultado =string.charCodeAt(0)-65+parseInt(offset,10)+65;
+    return String.fromCharCode(resultado);
   },
   decode: (offset, string) => {
     /* Acá va tu código que descifra*/
--- src/index.js
+++ src/index.js
@@ -11,18 +11,17 @@ const clicklisto = document.getElementById("listo");
 //se declara la variable para los intentos al ingresar PASSWORD
 let intentos = 2;
 
-
 //se ejecuta contraseña y los intentos
 clickBtn.addEventListener("click", ()=>{
     if(clave.value =="LABORATORIA" ){
       document.getElementById("acceso").classList.add("ocultar");
       document.getElementById("ingreso").classList.remove("ocultar");
-      
+  
     } else if(intentos ==0){
       alert("Ya utilizaste todos tus intentos, en este momento, no podrás seguir.");
       btn.disabled="disabled";
-
      }
+    
      else{
         alert("Clave incorrecta, te quedan "+ intentos + " intentos");
         intentos= intentos-1;
@@ -36,10 +35,11 @@ const coffset1 = document.getElementById("offset1");
 const smensaje2 = document.getElementById("mensaje2");
 const doffset2 = document.getElementById("offset2");
 
-//De ingreso a cifrado para cifrar 
+//De la pagina ingreso a la pagina decifrado
+// ademas capturar el cifrado 
+
 clickCifrar.addEventListener("click", ()=>{
  
-
   console.log(cipher.encode(coffset1.value,smensaje1.value));
 
   document.getElementById("acceso").classList.add("ocultar");
@@ -47,6 +47,7 @@ clickCifrar.addEventListener("click", ()=>{
   document.getElementById("cifrado").classList.remove("ocultar");
   document.getElementById("final").classList.add("ocultar");
 });
+
 //de ingreso a cifrado para descifrar
 clickDescifrar.addEventListener("click", ()=>{
   document.getElementById("acceso").classList.add("ocultar");
@@ -54,6 +55,7 @@ clickDescifrar.addEventListener("click", ()=>{
   document.getElementById("cifrado").classList.remove("ocultar");
   document.getElementById("final").classList.add("ocultar");
 });
+
 //Luego de cifrar o descifrar al final
 clicklisto.addEventListener("click", ()=>{
   document.getElementById("acceso").classList.add("ocultar");
